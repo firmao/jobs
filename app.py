5f855ce43f56f4c9853ee0838250c306f31a9439
Andre Valdestilhas <firmao@gmail.com>
2024-07-23 19:26:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -145,5 +145,7 @@ def uploaded_file(filename):
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.app_context().push()
     db.create_all()
     app.run(debug=True)
+
